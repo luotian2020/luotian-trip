ad416df832cd4363554eafe7f9813814ca2b7ecb
luotian2020 <luotian2020@users.noreply.github.com>
2026-08-02 14:16:53 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,163 @@
+/**
+ * 舟山花鸟岛拍摄行程规划数据
+ * position: [经度, 纬度] (高德坐标)
+ * scenes: 环境例图直链
+ * poses: 动作参考直链
+ */
+window.HUANIAO_MAP_DATA = [
+    {
+        id: 1,
+        position: [122.69783, 30.848415],
+        title: "南岙沙滩",
+        characters: [
+            {
+                name: "守岸人",
+                desc: "退潮的时候适合去拍，需要走到边上有部分礁石的地方",
+                scenes: ["./scene/nanaoshatan/nanaoshatan.jpg"],
+                poses: [
+                    "./pose/shatan/shouanren/1.JPEG",
+                    "./pose/shatan/shouanren/2.PNG",
+                    "./pose/shatan/shouanren/3.PNG",
+                    "./pose/shatan/shouanren/4.png",
+                    "./pose/shatan/shouanren/5.png",
+                    "./pose/shatan/shouanren/6.png",
+                    "./pose/shatan/shouanren/7.png",
+                    "./pose/shatan/shouanren/8.png",
+                    "./pose/shatan/shouanren/9.png",
+                    "./pose/shatan/shouanren/10.png",
+                    "./pose/shatan/shouanren/11.png",
+                    "./pose/shatan/shouanren/12.png",
+                    "./pose/shatan/shouanren/13.png",
+                    "./pose/shatan/shouanren/14.png",
+                    "./pose/shatan/shouanren/15.png",
+                    "./pose/shatan/shouanren/16.jpg",
+                    "./pose/shatan/shouanren/17.jpg",
+
+                ]
+            },
+            {
+                name: "美树沙耶香",
+                desc: "退潮的时候适合去拍，许多白房子也可以当背景，晚上可海岸附近拍",
+                scenes: ["./scene/nanaoshatan/nanaoshatan.jpg"],
+                poses: [
+                    "./pose/shatan/shayexiang/1.jpg",
+                    "./pose/shatan/shayexiang/2.png",
+                    "./pose/shatan/shayexiang/3.png",
+                    "./pose/shatan/shayexiang/4.png",
+                    "./pose/shatan/shayexiang/5.png",
+                    "./pose/shatan/shayexiang/6.png",
+                    "./pose/shatan/shayexiang/7.png",
+                    "./pose/shatan/shayexiang/8.png",
+                    "./pose/shatan/shayexiang/9.png",
+                    "./pose/shatan/shayexiang/10.png",
+                    "./pose/shatan/shayexiang/11.png",
+                    "./pose/shatan/shayexiang/12.png",
+                    "./pose/shatan/shayexiang/13.png",
+                    "./pose/shatan/shayexiang/14.png",
+                    "./pose/shatan/shayexiang/15.png",
+                ]
+            },
+            {
+                name: "爱莉泳装",
+                desc: "退潮的时候适合去拍",
+                scenes: ["./scene/nanaoshatan/nanaoshatan.jpg"],
+                poses: [
+                    "./pose/shatan/aili/1.png",
+                    "./pose/shatan/aili/2.png",
+                    "./pose/shatan/aili/3.jpg",
+                    "./pose/shatan/aili/4.png",
+                    "./pose/shatan/aili/5.png",
+                    "./pose/shatan/aili/6.png",
+                    "./pose/shatan/aili/7.png",
+                    "./pose/shatan/aili/8.png",
+                    "./pose/shatan/aili/9.png",
+                    "./pose/shatan/aili/10.png",
+                    "./pose/shatan/aili/11.png",
+                    "./pose/shatan/aili/12.png",
+                    "./pose/shatan/aili/13.png",
+
+                ]
+            },
+            {
+                name: "37",
+                desc: "退潮的时候适合去拍，许多白房子也可以当背景，晚上可海岸附近拍",
+                scenes: ["./scene/nanaoshatan/nanaoshatan.jpg"],
+                poses: [
+                    "./pose/shatan/37/1.png",
+                    "./pose/shatan/37/3.png",
+                    "./pose/shatan/37/4.png",
+                    "./pose/shatan/37/5.jpg",
+                    "./pose/shatan/37/6.jpg",
+                    "./pose/shatan/37/7.jpg",
+                    "./pose/shatan/37/8.jpg",
+                    "./pose/shatan/37/9.jpg",
+                    "./pose/shatan/37/10.jpg",
+                    "./pose/shatan/37/11.png",
+                    "./pose/shatan/37/12.png",
+                    "./pose/shatan/37/13.png",
+                    "./pose/shatan/37/14.png",
+                    "./pose/shatan/37/15.png",
+                    "./pose/shatan/37/16.png",
+                ]
+            },
+            {
+                name: "薇尔莉特",
+                desc: "退潮的时候适合去拍，礁石",
+                scenes: ["./scene/nanaoshatan/nanaoshatan.jpg"],
+                poses: [
+                    "./pose/shatan/weierlite/1.png",
+                    "./pose/shatan/weierlite/3.png",
+                    "./pose/shatan/weierlite/4.png",
+                    "./pose/shatan/weierlite/5.png",
+                    "./pose/shatan/weierlite/6.png",
+                    "./pose/shatan/weierlite/7.png",
+                    "./pose/shatan/weierlite/8.png",
+
+                ]
+            },
+        ]
+    },
+    {
+        id: 2,
+        position: [122.69547, 30.851743],
+        title: "致爱书房",
+        characters: [
+            {
+                name: "守岸人",
+                desc: "钢琴、门为主要外景场地",
+                scenes: ["./scene/zhiaishufang/gangqin.png", "./scene/zhiaishufang/men.png"],
+                poses: [
+                    "./pose/zhiaishufang/shouanren/1.png",
+                    "./pose/zhiaishufang/shouanren/2.png",
+                    "./pose/zhiaishufang/shouanren/3.png",
+                    "./pose/zhiaishufang/shouanren/4.png",
+                    "./pose/zhiaishufang/shouanren/5.png",
+                ]
+            },
+             {
+                name: "薇尔莉特",
+                desc: "走进白房子或者佛手石",
+                scenes: ["./scene/zhiaishufang/gangqin.png", "./scene/zhiaishufang/men.png"],
+                poses: [
+                    "./pose/zhiaishufang/weierlite/1.png",
+                    "./pose/zhiaishufang/weierlite/2.png",
+                ]
+            }
+        ]
+    },
+    {
+        id: 3,
+        position: [122.693911, 30.844471],
+        title: "玛塔草坪秋千",
+        characters: [
+            {
+                name: "守岸人",
+                desc: "守岸人坐在秋千上，从侧面向镜头伸出一只手，像是在邀请",
+                scenes: ["./scene/caopingqiuqian/qiuqian.jpg"],
+                poses: [
+                    "./pose/qiuqian/shouanren/1.jpg",
+                ]
+            }
+        ]
+    }
+];
